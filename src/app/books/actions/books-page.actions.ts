9697738a4c36497c85388d07d5e51eec08b613c6
Matt Smith <mattjsm@gmail.com>
2023-01-17 17:38:42 -0600

    stub out book page actions

--- src/app/books/actions/books-page.actions.ts
+++ src/app/books/actions/books-page.actions.ts
@@ -1,2 +1,8 @@
 import { createAction, props } from "@ngrx/store";
 import { BookRequiredProps } from "src/app/shared/models";
+
+export const createBook = createAction('[Books Page] Create Book', props<{ book: BookRequiredProps }>());
+export const selectBook = createAction('[Books Page] Select Book', props<{id: string, book: BookRequiredProps}>());
+export const editBook = createAction('[Books Page] Edit Book', props<{ id: string, book: BookRequiredProps }>());
+export const deleteBook = createAction('[Books Page] Delete Book', props<{id: string}>());
+export const clearSelection = createAction('[Books Page] Cancel Edit');
